f57c2d017994c20b6982016d70cfe4a4e94f8a1b
Austin Salguero <austinsalguero08@gmail.com>
2026-06-03 00:14:41 -0500

    Auto-generated commit

--- chunk-3Y2S6GVF.js
+++
@@ -1,1 +0,0 @@
-var e={production:!0,apiUrl:"https://asalguero.ifree.page/api"};export{e as a};
---
+++ chunk-WSKEQ7AL.js
@@ -0,0 +1,1 @@
+var p={production:!0,apiUrl:"https://seriusgamelumbalgia-production.up.railway.app/api"};export{p as a};
